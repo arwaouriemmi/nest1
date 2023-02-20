b66067151a13beb68c358db68c400784e8da551e
arwaouriemmi <arwaouriemmi@gmail.com>
2023-02-20 23:55:28 +0100

    first commit

---
+++ src/premier/premier.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PremierController } from './premier.controller';
+import { PremierService } from './premier.service';
+
+
+@Module({
+  controllers: [PremierController],
+  providers: [PremierService]
+  
+})
+export class PremierModule {}
